fda67379bbacb04c4e16a53b9f2f802004ec65c3
yousefhussien727 <yousef.hu@outlook.com>
2025-09-29 00:09:58 +0300

    Use Lombok Annotations for more clean code

--- src/main/java/com/yh/springstore/model/Category.java
+++ src/main/java/com/yh/springstore/model/Category.java
@@ -4,42 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 // @Entity(name = "CATEGORIES") // opt: to specify custom db table name
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
-
-
-    public Category(){
-
-    }
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-
-    public Long getCategoryId() {
-        return this.categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return this.categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-
+    
 }
