682648317d2fd6eb0ec77f0d45ed72a4e79cf6be
yousefhussien727 <yousef.hu@outlook.com>
2025-09-29 00:32:30 +0300

    remove unused imports

--- src/main/java/com/yh/springstore/service/CategoryServiceImpl.java
+++ src/main/java/com/yh/springstore/service/CategoryServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
