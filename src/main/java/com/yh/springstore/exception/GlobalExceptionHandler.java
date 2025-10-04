23a15a5ea8f569b77bcfd1386e91d30ac5179b65
yousefhussien727 <yousef.hu@outlook.com>
2025-10-04 16:12:11 +0300

    remove unused imports

--- src/main/java/com/yh/springstore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yh/springstore/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
