d475c081ebd87ba9cfc230971245629ab1f1cafa
yousefhussien727 <yousef.hu@outlook.com>
2025-10-06 15:45:42 +0300

    Add Sorting constants

--- src/main/java/com/yh/springstore/config/AppConstants.java
+++ src/main/java/com/yh/springstore/config/AppConstants.java
@@ -3,4 +3,6 @@ package com.yh.springstore.config;
 public class AppConstants {
     public static final String PAGE_NUMBER = "0";
     public static final String PAGE_SIZE = "20";
+    public static final String SORT_CATEGORY_BY = "categoryId";
+    public static final String SORT_DIR = "ASC";
 }
