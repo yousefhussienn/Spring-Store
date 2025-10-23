93f0a51abfb2f5cbbb20a6442b6ad5345cd1b70d
yousefhussien727 <yousef.hu@outlook.com>
2025-10-23 11:33:54 +0300

    Saving Product to a Category -Controller

--- src/main/java/com/yh/springstore/controller/ProductController.java
+++ src/main/java/com/yh/springstore/controller/ProductController.java
@@ -1,17 +1,30 @@
 package com.yh.springstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yh.springstore.payload.ProductDTO;
+import com.yh.springstore.payload.ProductResponse;
 import com.yh.springstore.service.ProductService;
 
 @RestController
-@RequestMapping("/someshit")
+@RequestMapping("/api")
 public class ProductController {
-    
+
     @Autowired
     private ProductService productService;
 
-    
+    @PostMapping("/admin/categories/{categoryId}/products")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO, @PathVariable Long categoryId) {
+        ProductDTO savedProductDTO = productService.addProduct(productDTO, categoryId);
+        return new ResponseEntity<>(savedProductDTO, HttpStatus.CREATED);
+    }
+
 }
