288cff41586cf019c7a92f3004bf09d60a72c9ee
yousefhussien727 <yousef.hu@outlook.com>
2025-11-22 02:03:23 +0200

    Refactor LoginRequest class

--- src/main/java/com/yh/springstore/security/jwt/LoginRequest.java
+++ src/main/java/com/yh/springstore/security/jwt/LoginRequest.java
@@ -1,23 +1,14 @@
 package com.yh.springstore.security.jwt;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class LoginRequest {
+    @NotBlank
     private String username;
-
+    @NotBlank
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
