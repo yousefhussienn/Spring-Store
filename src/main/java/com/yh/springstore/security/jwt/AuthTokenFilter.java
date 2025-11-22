b314373adc0cd829611a910640484d25cd8a5730
yousefhussien727 <yousef.hu@outlook.com>
2025-11-22 16:42:21 +0200

    Use my custom UserDetailsServiceImpl for user details retrieval instead of the default

--- src/main/java/com/yh/springstore/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/yh/springstore/security/jwt/AuthTokenFilter.java
@@ -10,10 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.yh.springstore.security.services.UserDetailsServiceImpl;
 
 import java.io.IOException;
 
@@ -23,7 +24,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     private JwtUtils jwtUtils;
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserDetailsServiceImpl userDetailsService;
 
     private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
