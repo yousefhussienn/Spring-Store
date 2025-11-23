8a251cdc71b25fb1117de0b1bd92f35d631a335a
yousefhussien727 <yousef.hu@outlook.com>
2025-11-23 18:57:10 +0200

    Fix by replace @NotBlank with @NotNull for roleName validation in Role class

--- src/main/java/com/yh/springstore/model/Role.java
+++ src/main/java/com/yh/springstore/model/Role.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,10 +22,10 @@ public class Role {
     private Long roleId;
 
     @Enumerated(EnumType.STRING)
-    @NotBlank
+    @NotNull
     private UserRole roleName;
 
-    public Role(@NotBlank UserRole roleName) {
+    public Role(@NotNull UserRole roleName) {
         this.roleName = roleName;
     }
     
