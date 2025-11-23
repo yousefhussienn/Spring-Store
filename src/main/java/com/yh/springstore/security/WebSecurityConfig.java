1edad6599ab1364a6df6524b43158e54b62a2b9b
yousefhussien727 <yousef.hu@outlook.com>
2025-11-23 18:52:30 +0200

    Fix method name in WebSecurityConfig to use 'findByUsername' for user retrieval

--- src/main/java/com/yh/springstore/security/WebSecurityConfig.java
+++ src/main/java/com/yh/springstore/security/WebSecurityConfig.java
@@ -143,17 +143,17 @@ public class WebSecurityConfig {
             }
 
             // Update roles for existing users
-            userRepository.findByUserName("user1").ifPresent(user -> {
+            userRepository.findByUsername("user1").ifPresent(user -> {
                 user.setRoles(userRoles);
                 userRepository.save(user);
             });
 
-            userRepository.findByUserName("seller1").ifPresent(seller -> {
+            userRepository.findByUsername("seller1").ifPresent(seller -> {
                 seller.setRoles(sellerRoles);
                 userRepository.save(seller);
             });
 
-            userRepository.findByUserName("admin").ifPresent(admin -> {
+            userRepository.findByUsername("admin").ifPresent(admin -> {
                 admin.setRoles(adminRoles);
                 userRepository.save(admin);
             });
