6e24cdee51074e1deab11ec874867e37f620d5e7
yousefhussien727 <yousef.hu@outlook.com>
2026-03-26 19:16:41 +0200

    222. (FIX) Move CategoryServiceImpl class to the correct path

--- src/main/java/com/yh/springstore/service/CategoryServiceImpl.java
+++ src/main/java/com/yh/springstore/service/CategoryServiceImpl.java
@@ -1,4 +1,4 @@
-package com.yh.springstore.exception;
+package com.yh.springstore.service;
 
 import java.util.List;
 
@@ -12,11 +12,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.yh.springstore.exception.APIException;
+import com.yh.springstore.exception.ResourceNotFoundException;
 import com.yh.springstore.model.Category;
 import com.yh.springstore.payload.CategoryDTO;
 import com.yh.springstore.payload.CategoryResponse;
 import com.yh.springstore.repository.CategoryRepository;
-import com.yh.springstore.service.CategoryService;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
