46bc453dfb8acaf5fef5bdd07dab4548812b0054
yousefhussien727 <yousef.hu@outlook.com>
2026-03-27 15:48:06 +0200

    225. (Fix) Change findCartByEmail method to return Optional<Cart>

--- src/main/java/com/yh/springstore/repository/CartRepository.java
+++ src/main/java/com/yh/springstore/repository/CartRepository.java
@@ -1,5 +1,7 @@
 package com.yh.springstore.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,5 +10,5 @@ import com.yh.springstore.model.Cart;
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
-    Cart findCartByEmail(String userEmail);
+    Optional<Cart> findCartByEmail(String userEmail);
 }
