2b702bb2caa2b3c40f70499db317a3a229dda8c8
yousefhussien727 <yousef.hu@outlook.com>
2026-04-06 00:52:22 +0200

    250. (Feature) Add Order, OrderItem, and Payment entities with relationships

---
+++ src/main/java/com/yh/springstore/model/Order.java
@@ -0,0 +1,54 @@
+package com.yh.springstore.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "order", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDate orderDate;
+    
+    @OneToOne
+    @JoinColumn(name = "payment_id")
+    private Payment payment;
+
+    private Double totalAmount;
+    private String orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+}
---
+++ src/main/java/com/yh/springstore/model/Payment.java
@@ -0,0 +1,50 @@
+package com.yh.springstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "order_items")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @NotBlank
+    @Size(min = 4, message = "Payment method must contain at least 4 characters")
+    private String paymentMethod;
+	
+    private String pgPaymentId;
+    private String pgStatus;
+    private String pgResponseMessage;
+    private String pgName;
+
+    // Custom Constructor
+    public Payment(String paymentMethod, String pgPaymentId, String pgStatus,
+                   String pgResponseMessage, String pgName) {
+        this.paymentMethod = paymentMethod;
+        this.pgPaymentId = pgPaymentId;
+        this.pgStatus = pgStatus;
+        this.pgResponseMessage = pgResponseMessage;
+        this.pgName = pgName;
+    }
+    
+}
